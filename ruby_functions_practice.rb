fe65818eae8d8632bc3ab465d6d0f48bf7c2bfd9
Adrian Tuckwell <adrian.tuckwell@gmail.com>
2016-09-27 16:10:03 +0100

    initial commit

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,82 @@
+
+
+def return_10()
+  return 10
+end
+
+def add (num_1, num_2)
+  return num_1 + num_2
+end
+
+def subtract (num_1, num_2)
+  return num_1 - num_2
+end
+
+def multiply (num_1, num_2)
+  return num_1 * num_2
+end
+
+def divide (num_1, num_2)
+  return num_1 / num_2
+end
+
+def length_of_string( test_string )
+  return test_string.length
+end
+
+def join_string( string_1, string_2 )
+  return string_1 + string_2
+end
+
+def add_string_as_number( num_1, num_2 )
+ return num_1.to_i + num_2.to_i
+end
+
+def number_to_full_month_name( month )
+  case month
+      when 1
+        return "January"
+      when 3
+        return "March"
+      when 9
+        return "September"
+  end
+end
+
+def number_to_short_month_name( month )
+  case month
+      when 1
+        return "Jan"
+      when 3
+        return "Mar"
+      when 9
+        return "Sep"
+  end
+end
+
+def volume_of_cube( num )
+  return num * num
+end
+
+def volume_of_sphere( radius )
+  val = 2 * Math::PI * radius
+  return val.round(2)
+end
+
+def fahrenheit_to_celsius( num )
+#(°F  -  32)  x  5/9 = °C
+  val  = (num - 32) * 5/9
+  return val.round(2)
+end
+
+def miles_to_kilometres( num )
+# km = m * 1.6
+  return num * 1.6
+  
+end
+
+
+
+
+
+
